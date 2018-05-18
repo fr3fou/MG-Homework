34d633b0c7ddbde9b469b6e700fdc506051ecf56
Simo Aleksandrov <simo3003@me.com>
2018-05-18 08:09:59 +0300

    Add GeniusCounter

---
+++ Informatika/GeniusCounter/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GeniusCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Properties.Settings.Default.counter++;
+                Properties.Settings.Default.lastTimeSaid = DateTime.Now;
+                Properties.Settings.Default.Save();
+                label1.Text = Properties.Settings.Default.counter.ToString();
+                label2.Text = "Last time said: " + Properties.Settings.Default.lastTimeSaid;
+            }
+            else if (e.Control && e.KeyCode == Keys.Delete)
+            {
+                Properties.Settings.Default.counter = 0;
+                Properties.Settings.Default.lastTimeSaid = DateTime.Now;
+                Properties.Settings.Default.Save();
+                label1.Text = Properties.Settings.Default.counter.ToString();
+                label2.Text = "Last time said: " + "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = Properties.Settings.Default.counter.ToString();
+            label2.Text = "Last time said: " + Properties.Settings.Default.lastTimeSaid;
+        }
+    }
+}
