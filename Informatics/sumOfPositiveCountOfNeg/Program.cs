2663400cab9f009bf24e383b5103b48b57495365
Simo Aleksandrov <simo3003@me.com>
2018-09-27 01:58:00 +0300

    Add hwk

---
+++ Informatics/sumOfPositiveCountOfNeg/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sumOfPositiveCountOfNeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int evenAmount = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > 0)
+                {
+                    sum += num;
+                }
+
+                if (num % 2 == 0)
+                {
+                    evenAmount++;
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(evenAmount);
+        }
+    }
+}
