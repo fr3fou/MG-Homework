9fb0474819a6c905eb0c02a4cec90c4ae63667b5
Simo Aleksandrov <simo3003@me.com>
2018-11-19 09:04:07 +0200

    Add 2d

---
+++ Informatics/2D/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2D
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("N: ");
+			int n = int.Parse(Console.ReadLine());
+			Console.Write("M: ");
+			int m = int.Parse(Console.ReadLine());
+			int[, ] _2d = new int[n, m];
+			int max = _2d[0, 0];
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					Console.Write($"arr[{i}][{j}]: ");
+					_2d[i, j] = int.Parse(Console.ReadLine());
+					if (_2d[i, j] > max)
+					{
+						max = _2d[i, j];
+					}
+				}
+			}
+
+			Console.WriteLine($"Max: {max}");
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					Console.Write(_2d[i, j] + " ");
+				}
+
+				Console.WriteLine();
+			}
+		}
+	}
+}
