6b2279e533c9ed3661c5942e237f9276e1aebcc2
Simo Aleksandrov <simo3003@me.com>
2019-01-30 15:18:49 -0800

    Add Vacation 2.0

---
+++ Informatics/Vacation-2.0/M1-20/M1-20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M1_20
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            // размер на масив5
+            Console.Write("Razmer na masivi: ");
+            int n = int.Parse(Console.ReadLine()); 
+
+            // начална стойност на произведението
+            double mul = 1;
+            
+            // декларация на двата масива
+            double[] arr1 = new double[n];
+            double[] arr2 = new double[n];
+
+            // попълване на първия масив 
+            Console.WriteLine("Purvi masiv: ");
+            for (int i = 0; i < n; i++)
+            {
+                arr1[i] = double.Parse(Console.ReadLine());
+            }
+
+            // попълване на втория масив 
+            Console.WriteLine("Vtori masiv: ");
+            for (int i = 0; i < n; i++)
+            {
+                arr2[i] = double.Parse(Console.ReadLine());
+            }
+
+            // преминаване на масива и умножаване на всички НОД
+            for (int i = 0; i < n; i++)
+            {
+                // калкулиране на НОД от числата в двата масива
+                mul *= GCD(arr1[i], arr2[i]);
+            }
+
+            // принтиране на произведението
+            Console.Write("Rezultat: ");
+            Console.WriteLine(mul);
+        }
+
+       // пресмятане на НОД на две числа чрез рекурсия (алгоритъм на Евклид)
+       private static double GCD(double a, double b) 
+       { 
+           return (b == 0) ? a : GCD(b, a % b);
+       } 
+    }
+}
